ffef2b39aac0b31cdd64073c656fbedb0bcdcbb9
abranllis <47560487+abranllis@users.noreply.github.com>
2023-02-08 18:50:47 +0100

    1

--- src/matriculaUniversidadSemaforos.java
+++ src/matriculaUniversidadSemaforos.java
@@ -1,8 +1,9 @@
 import java.util.concurrent.Semaphore;
 
 class matriculaUniversidadSemaforos {
-    private static int NIP = 1001;
-    private static Semaphore semaphore = new Semaphore(3);
+    private static int NIP = 1;
+    private static Semaphore semaphore = new Semaphore(3); //Hay 3 mostradores para matricularse
+
 
     static class Student extends Thread {
         private int matricula;
@@ -14,7 +15,7 @@ class matriculaUniversidadSemaforos {
         public void run() {
             try {
                 semaphore.acquire();
-                System.out.println("Alumno " + matricula + " matriculado con NIP " + NIP);
+                System.out.println("Alumno con matricula " + matricula + " matriculado con numero: " + NIP);
                 NIP++;
                 Thread.sleep(1000);
                 semaphore.release();
@@ -25,7 +26,9 @@ class matriculaUniversidadSemaforos {
     }
 
     public static void main(String[] args) {
-        for (int i = 1001; i <= 1020; i++) {
+        //Matriculamos a los estudiantes
+        Integer numEstudiates = 50; //numero de estudiantes que estan en la sala de espera
+        for (int i = 1; i <= numEstudiates; i++) {
             new Student(i).start();
         }
     }
