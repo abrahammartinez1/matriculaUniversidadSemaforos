d5464c1539ddfc45d2d5ebd04a4ca4268c2ec0ae
abranllis <47560487+abranllis@users.noreply.github.com>
2023-02-09 13:35:34 +0100

    1

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 class Estudiante extends Thread {
 
@@ -11,12 +12,14 @@ class Estudiante extends Thread {
         this.identif = identif;
         this.itv = itv;
 
-        this.NIP = identif;
+        this.NIP = identif * 1050;
         this.matricula = itv;
+       // this.numMatricula = 1;
     }
 
     @Override
     public void run() {
-        itv.nuevoCoche(identif);
+        //itv.nuevoCoche(identif);
+        itv.nuevoCoche(this.NIP);
     }
 }
