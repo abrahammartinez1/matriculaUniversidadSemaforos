42d10c203ed704a26499ba4ceee15e68d7317772
abranllis <47560487+abranllis@users.noreply.github.com>
2023-02-09 10:17:26 +0100

    1

--- src/matriculaUniversidadSemaforos.java
+++ src/matriculaUniversidadSemaforos.java
@@ -3,31 +3,23 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class matriculaUniversidadSemaforos {
-    public static int NIP = 1;
+
     private static Semaphore semaphore = new Semaphore(3); //Hay 3 mostradores para matricularse
 
 
     static class Student extends Thread {
         private int matricula;
-
+        public static int NIP = 1;
         Student(int matricula) {
             this.matricula = matricula;
         }
 
         public void run() {
-            Lock lock = new ReentrantLock();
+
             try {
                 semaphore.acquire();
-
-                lock.lock();
-                try {
-                    // Aquí coloca la línea de código que solo quieres que ejecute un hilo a la vez
-                    System.out.println("Alumno con matricula " + matricula + " matriculado con numero: " + NIP);
-                    NIP++;
-                } finally {
-                    lock.unlock();
-                }
-
+                System.out.println("Alumno con matricula " + matricula + " matriculado con numero: " + NIP);
+                NIP++;
                 Thread.sleep(1000);
                 semaphore.release();
             } catch (InterruptedException e) {
@@ -38,7 +30,7 @@ class matriculaUniversidadSemaforos {
 
     public static void main(String[] args) {
         //Matriculamos a los estudiantes
-        Integer numEstudiates = 50; //numero de estudiantes que estan en la sala de espera
+        Integer numEstudiates = 10; //numero de estudiantes que estan en la sala de espera
         for (int i = 1; i <= numEstudiates; i++) {
             new Student(i).start();
         }
